3415753ab3ed23b550d57e7038de543c3a4b0153
youngyoung0 <taeyoung6873@gmail.com>
2023-01-19 00:21:38 +0900

    프로젝트 초기설정

---
+++ fopy/src/main/java/com/fopy/FopyApplication.java
@@ -0,0 +1,13 @@
+package com.fopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FopyApplication.class, args);
+	}
+
+}
---
+++ fopy/src/test/java/com/fopy/FopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
